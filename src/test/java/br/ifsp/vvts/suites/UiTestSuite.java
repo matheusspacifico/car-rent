874faf98d4ca4e74af9e65214c57ff9dcf8f7d66
matheusspacifico <matheuspacifico44@gmail.com>
2025-11-30 14:39:26 -0300

    test: add ui test suite

---
+++ src/test/java/br/ifsp/vvts/suites/UiTestSuite.java
@@ -0,0 +1,12 @@
+package br.ifsp.vvts.suites;
+
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("br.ifsp.vvts")
+@IncludeTags("UiTest")
+public class UiTestSuite {
+}
+
