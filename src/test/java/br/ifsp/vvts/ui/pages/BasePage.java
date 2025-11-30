9afb99226c09d8a642f9be72aab959c421e2c7fc
matheusspacifico <matheuspacifico44@gmail.com>
2025-11-30 14:41:39 -0300

    test: add base page for ui tests

---
+++ src/test/java/br/ifsp/vvts/ui/pages/BasePage.java
@@ -0,0 +1,75 @@
+package br.ifsp.vvts.ui.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public abstract class BasePage {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    private static final Duration DEFAULT_TIMEOUT = Duration.ofSeconds(10);
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, DEFAULT_TIMEOUT);
+    }
+
+    protected WebElement waitForElement(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected WebElement waitForClickable(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    protected void type(By locator, String text) {
+        WebElement element = waitForElement(locator);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    protected void click(By locator) {
+        waitForClickable(locator).click();
+    }
+
+    protected String getText(By locator) {
+        return waitForElement(locator).getText();
+    }
+
+    protected boolean isElementVisible(By locator) {
+        try {
+            return waitForElement(locator).isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    protected boolean isElementEnabled(By locator) {
+        try {
+            return waitForElement(locator).isEnabled();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    protected boolean waitForUrl(String urlFragment) {
+        return wait.until(ExpectedConditions.urlContains(urlFragment));
+    }
+
+    protected void waitForPageLoad() {
+        wait.until(driver -> 
+            driver.findElement(By.tagName("body")).isDisplayed()
+        );
+    }
+
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+}
+
