3a1e2f5d734ae273073bd9ccc08ceb3c0f32f426
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-11-30 21:01:32 -0300

    test: create test to check create customer process

--- src/test/java/br/ifsp/vvts/ui/CustomerPageTest.java
+++ src/test/java/br/ifsp/vvts/ui/CustomerPageTest.java
@@ -1,5 +1,6 @@
 package br.ifsp.vvts.ui;
 
+import br.ifsp.vvts.ui.pages.CustomerFormPage;
 import br.ifsp.vvts.ui.pages.CustomerPage;
 import org.junit.jupiter.api.*;
 
@@ -29,6 +30,40 @@ public class CustomerPageTest extends AuthenticatedBaseUiTest {
         @DisplayName("Should display Add button")
         public void shouldDisplayAddButton() {
             assertThat(customerPage.isAddCustomerButton()).isTrue();
+        }
+    }
+
+    @Nested
+    @DisplayName("Customer Operations Tests")
+    class OperationsTests {
+
+        @Test
+        @DisplayName("Should create a new customer successfully")
+        public void shouldCreateCustomer() {
+            String name = faker.name().fullName();
+            String cpf = "035.056.850-20";
+
+            CustomerFormPage form = customerPage.clickAddCustomerButton();
+            form.fillForm(name, cpf);
+            customerPage = form.clickSubmit();
+
+            assertThat(customerPage.isCustomerListed(cpf)).isTrue();
+
+            assertThat(customerPage.checkCustomerRowData(name, cpf)).isTrue();
+        }
+
+        @Test
+        @DisplayName("Should cancel process to create a new customer")
+        public void shouldCancelCreateCustomer() {
+            String name = faker.name().fullName();
+            String cpf = "035.056.850-20";
+
+            CustomerFormPage form = customerPage.clickAddCustomerButton();
+            form.fillForm(name, cpf);
+            customerPage = form.clickCancel();
+
+            assertThat(customerPage.isCustomerListed(cpf)).isFalse();
+            assertThat(customerPage.checkCustomerRowData(name, cpf)).isFalse();
         }
     }
 }
